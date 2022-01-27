509fb17162e0e32daaf5eaaacd7bb61f3efd07f4
delorme38 <83075378+delorme38@users.noreply.github.com>
2022-01-26 21:41:58 -0500

    mise a jour

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-// if ('serviceWorker' in navigator) {
-//    navigator.serviceWorker.register('/sw.js').then(function(reg) {
-//      // registration worked
-//      console.log('Registration succeeded. Scope is ' + reg.scope);
-//    }).catch(function(error) {
-//      // registration failed
-//      console.log('Registration failed with ' + error);
-//    });
-//  }
-
  if ('serviceWorker' in navigator) {
-   navigator.serviceWorker.register('./sw.js').then(function(reg) {
+   navigator.serviceWorker.register('sw.js').then(function(reg) {
  
      if(reg.installing) {
        console.log('Service worker installing');
--- sw.js
+++ sw.js
@@ -3,37 +3,32 @@ self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open('v1').then((cache) => {
             return cache.addAll([
-                './index.html',
-                './man/manifest.json',
-                './index.js',
-                './sw.js',
-                './Carte.js',
-                './favicon.ico',
+                'index.html',
+                'manifest.json',
+                'index.js',
+                'sw.js',
+                'Carte.js',
+                'favicon.ico',
                 'bootstrap-5.1.3-dist/css/bootstrap.min.css',
-                './icons-1.7.2/font/bootstrap-icons.css',
-                './bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
-                './img/img-1.jpg',
-                './img/img-2.jpg',
-                './img/img-3.jpg',
-                './img/img-4.jpg',
-                './img/img-5.jpg',
-                './img/img-6.jpg',
-                './img/img-7.jpg',
-                './img/img-8.jpg',
-                './icon-192x192.png',
-                './icon-256x256.png',
-                './icon-384x384.png',
-                './icon-512x512.png'
+                'icons-1.7.2/font/bootstrap-icons.css',
+                'bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
+                'img/img-1.jpg',
+                'img/img-2.jpg',
+                'img/img-3.jpg',
+                'img/img-4.jpg',
+                'img/img-5.jpg',
+                'img/img-6.jpg',
+                'img/img-7.jpg',
+                'img/img-8.jpg',
+                'icon-192x192.png',
+                'icon-256x256.png',
+                'icon-384x384.png',
+                'icon-512x512.png'
             ]);
         })
     );
 
 })
-// self.addEventListener('fetch', function (event) {
-//     event.respondWith(
-//         caches.match(event.request)
-//     );
-// });
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(caches.match(event.request).then(function(response) {
@@ -53,7 +48,7 @@ self.addEventListener('fetch', function(event) {
           });
           return response;
         }).catch(function () {
-          return caches.match('/sw-test/gallery/myLittleVader.jpg');
+          return caches.match('img.jpg');
         });
       }
     }));
